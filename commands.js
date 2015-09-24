6c0d155ec07bd4843c5814a349dcba1ee20904c7
Andrew Tynefield <atynefield@gmail.com>
2015-09-24 01:10:16 -0400

    Reorganize command structure.

--- commands.js
+++ commands.js
@@ -1,30 +1,50 @@
-
-
-
 var commands = {
+
+  //TODO: loadModule command to load commands in from external modules.
+
+  // Utilities
 
   resolve: function (msg, callback) {
     var args = msg.text.split(' ');
-    if(typeof this[args[0]] == 'function') {
-      this[args[0]](msg, args.slice(1), callback);
+    if(typeof this.commands[args[0]] == 'function') {
+      this.commands[args[0]](msg, args.slice(1), callback);
     }
     else {
       callback('Command [' + msg.text + '] not found.');
     }
   },
 
-  '/wot': function(msg, args, callback) {
-    callback(null, 'wut');
-  },
-  '/hi': function(msg, args, callback) {
-    var from = msg.from.username ? '@' + msg.from.username : msg.from.first_name;
-    callback(null, 'Hello, ' + from + '!');
-  },
-  '/me': function (msg, args, callback) {
-    var from = msg.from.username ? '@' + msg.from.username : msg.from.first_name;
-    callback(null, '_' + from + ' ' + args.join(' ') + '_', { parse_mode: 'Markdown' });
-  }
 
+  /*
+ *  Command format:
+ *
+ *    '/command': function(msg, args, callback) {
+ *      // Do something
+ *      callback( // Error message, // Outbound message, // Object of message options );
+ *    }
+ */
+
+  commands: {
+
+    // 	Misc.
+
+    '/wot': function(msg, args, callback) {
+      callback(null, 'wut');
+    },
+
+    // Greetings
+    '/hi': function(msg, args, callback) {
+      var from = msg.from.username ? '@' + msg.from.username : msg.from.first_name;
+      callback(null, 'Hello, ' + from + '!');
+    },
+
+    // Actions
+    '/me': function (msg, args, callback) {
+      var from = msg.from.username ? '@' + msg.from.username : msg.from.first_name;
+      callback(null, '_' + from + ' ' + args.join(' ') + '_', { parse_mode: 'Markdown' });
+    }
+
+  }
 
 };
 
