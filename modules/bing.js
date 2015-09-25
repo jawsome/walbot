c30e355aea108962d1bba52c2f3537c8cb28a2ac
Andrew Tynefield <atynefield@gmail.com>
2015-09-24 23:29:22 -0400

    Implement reply_to_message on bing

--- modules/bing.js
+++ modules/bing.js
@@ -10,15 +10,15 @@ var bing = {
          console.log(err);
         }
         else if(body.d.results[0]) {
-        var image = body.d.results[0];
-        if(image.FileSize < 5000000) {
-          var data = request(image.MediaUrl);
-          bot.sendPhoto(msg.chat.id, data, { reply_to_message_id: msg.message_id });
-          callback(null, image.MediaUrl);
-        }
+          var image = body.d.results[0];
+          if(image.FileSize < 5000000) {
+            var data = request(image.MediaUrl);
+            bot.sendPhoto(msg.chat.id, data, { reply_to_message_id: msg.message_id });
+            callback(null, image.MediaUrl);
+          }
         }
         else {
-          bot.sendMessage(msg.chat.id, "u dun fukt up\njk bing ain't say shit tho");
+          bot.sendMessage(msg.chat.id, "u dun fukt up\njk bing ain't say shit tho", { reply_to_message_id: msg.message_id });
         }
       });
     }
