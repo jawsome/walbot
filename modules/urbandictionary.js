6b30ddbd3ac803b4dc32f97bbaba2df35bd84ecc
Andrew Tynefield <atynefield@gmail.com>
2015-09-25 14:05:15 -0400

    Handle no results on urbandictionary

--- modules/urbandictionary.js
+++ modules/urbandictionary.js
@@ -5,19 +5,30 @@ var ud = {
     '/ud': function(bot, msg, args, callback) {
       var search = urban(args.join(' '));
       search.first(function(json) {
-        var message = [
-          'Word: ' + json.word,
-          'Def.: ' + json.definition,
-          'Ex.: ' + json.example,
-          json.permalink
-        ];
-        bot.sendMessage(msg.chat.id,
-          message.join('\n'),
+        if(json !== undefined) {
+          console.log(json);
+          var message = [
+            'Word: ' + json.word,
+            'Def.: ' + json.definition,
+            'Ex.: ' + json.example,
+            json.permalink
+          ];
+          bot.sendMessage(msg.chat.id,
+            message.join('\n'),
+            {
+              reply_to_message_id: msg.message_id,
+              disable_web_page_preview: true
+            });
+          callback(null, json.permalink);
+        }
+        else {
+          bot.sendMessage(msg.chat.id, 'fuk u urbna dictionary\nsry ' + msg.from.first_name + ' he no know',
           {
             reply_to_message_id: msg.message_id,
             disable_web_page_preview: true
           });
-        callback(null, json.permalink);
+          callback(json, null);
+        }
       });
     }
   }
