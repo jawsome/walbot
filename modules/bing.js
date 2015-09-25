7e515bc484ecb212f2af72e45491b3eb2e8ccfbc
Andrew Tynefield <atynefield@gmail.com>
2015-09-24 23:14:59 -0400

    Resolve extension issues and implement reply

--- modules/bing.js
+++ modules/bing.js
@@ -1,29 +1,24 @@
 var config = require('./bing.config.json');
 var Bing = require('node-bing-api')({ accKey: config.key });
 var request = require('request');
-var fs = require('fs');
-var uuid = require('node-uuid');
 
 var bing = {
   commands: {
     '/img': function(bot, msg, args, callback) {
-      Bing.images(args.join(" "), { top: 1}, function (err, res, body) {
+      Bing.images(args.join(" "), { top: 5}, function (err, res, body) {
         if(err) {
          console.log(err);
         }
         else if(body.d.results[0]) {
         var image = body.d.results[0];
         if(image.FileSize < 5000000) {
-          var name = image.MediaUrl.split("/");
-          var name = uuid.v4() + name[name.length -1];
-          request(image.MediaUrl).pipe(fs.createWriteStream("../tmp/" + name)).on('close', function () {
-            bot.sendPhoto(msg.chat.id, "../tmp/" + name, { caption: args.join(" ") } );
-            callback(null, image.MediaUrl);
-          });
+          var data = request(image.MediaUrl);
+          bot.sendPhoto(msg.chat.id, data, { reply_to_message_id: msg.message_id });
+          callback(null, image.MediaUrl);
         }
         }
         else {
-          bot.sendMessage(msg.chat.id, "u dun fukt up");
+          bot.sendMessage(msg.chat.id, "u dun fukt up\njk bing ain't say shit tho");
         }
       });
     }
