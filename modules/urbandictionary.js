0aa0ef248073d25f05254fd0ad1df258c70c7b80
Andrew Tynefield <atynefield@gmail.com>
2015-12-05 23:04:06 -0500

    Support random UrbanDictionary

--- modules/urbandictionary.js
+++ modules/urbandictionary.js
@@ -1,4 +1,5 @@
 var urban = require('urban');
+var request = require('request');
 
 var ud = {
   commands: {
@@ -28,6 +29,39 @@ var ud = {
             disable_web_page_preview: true
           });
           callback(json, null);
+        }
+      });
+    },
+    '/udrand': function(bot, msg, args, callback) {
+      request('http://api.urbandictionary.com/v0/random', { json: true },
+        function(err, res, body) {
+          if(err) {
+            callback(err, null);
+            bot.sendMessage(msg.chat.id, 'fuk u urbna dictionary\nsry ' + msg.from.first_name + ' he no know',
+              {
+                reply_to_message_id: msg.message_id,
+                disable_web_page_preview: true
+              }
+            );
+         }
+         else {
+           if(body.list.length > 0) {
+             var json = body.list[Math.floor(Math.random() * (body.list.length - 1 + 1))];
+             var message = [
+              'Random word!',
+              'Word: ' + json.word,
+              'Def.: ' + json.definition,
+              'Ex.: ' + json.example,
+              json.permalink
+            ];
+            bot.sendMessage(msg.chat.id,
+              message.join('\n'),
+              {
+                reply_to_message_id: msg.message_id,
+                disable_web_page_preview: true
+              });
+            callback(null, json.permalink);
+          }
         }
       });
     }
